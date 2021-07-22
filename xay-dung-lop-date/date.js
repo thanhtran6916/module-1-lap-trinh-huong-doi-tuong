9be5a7fe9ea663d9c54056785b3ecccc0c117c83
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-07-22 14:05:33 +0700

    xay dung lop date

---
+++ xay-dung-lop-date/date.js
@@ -0,0 +1,39 @@
+class Date {
+    day;
+    month;
+    year;
+    constructor(day, month, year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    getDay() {
+        return this.day;
+    }
+    getMonth() {
+        return this.month;
+    }
+    getYear() {
+        return this.year;
+    }
+    setDay(day) {
+        return this.day = day;
+    }
+    setMonth(month) {
+        return this.month = month;
+    }
+    setYear(year) {
+        return this.year = year;
+    }
+    setDate(day, month, year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    tostring() {
+        return this.day + "/" + this.month + "/" + this.year;
+    }
+}
+let date = new Date(22, 7, 2021);
+document.write(date.tostring())
+
